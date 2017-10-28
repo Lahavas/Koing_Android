bfd0dd579e2ac83d658aab8a2bc2fd06fd2d4032
skysmile3 <isaaclee1101@hanmail.net>
2017-10-28 17:53:52 +0900

    add MyPageNameDialog by rascal

---
+++ app/src/main/java/com/tourwith/koing/Dialog/MyPageNameDialog.java
@@ -0,0 +1,114 @@
+package com.tourwith.koing.Dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.tourwith.koing.R;
+
+/**
+ * Created by MSI on 2017-10-27.
+ */
+
+public class MyPageNameDialog extends Dialog{
+    private static final String[] nations ={"United Kingdom","Korea","Japan","China","Spain","France"};
+    private static final String[] lans = {"English", "Korean", "Japanese", "Chinese", "Spanish", "French"};
+    private Context context;
+    private static final int LAYOUT = R.layout.mypage_name_dialog;
+    private TextView nameOKTextView;
+    private Spinner nationalitySpinner;
+    private Spinner nativeLanguageSpinner;
+    private TextView nationalityTextView;
+    private TextView nativeLanguageTextView;
+    private EditText profileNameEditText;
+    private TextView profileNameTextView;
+    private TextView profileNationLanguageTextView;
+
+
+    public MyPageNameDialog(@NonNull Context context, TextView profileNameTextView, TextView profileNationLanguageTextView){
+        super(context);
+        this.context = context;
+        this.profileNameTextView = profileNameTextView;
+        this.profileNationLanguageTextView = profileNationLanguageTextView;
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(LAYOUT);
+
+        nationalitySpinner = (Spinner)findViewById(R.id.nationality_spinner);
+        nativeLanguageSpinner = (Spinner)findViewById(R.id.native_language_spinner);
+        nationalityTextView = (TextView)findViewById(R.id.nationality_text_view);
+        nameOKTextView = (TextView)findViewById(R.id.name_ok_text_view);
+        nativeLanguageTextView = (TextView)findViewById(R.id.native_language_text_view);
+        profileNameEditText = (EditText)findViewById(R.id.profile_name_edit_text);
+
+        profileNameEditText.setText(profileNameTextView.getText().toString());
+
+        String s = profileNationLanguageTextView.getText().toString();
+        String nation="",language="";
+        if(s!=null) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '-') {
+                    nation = s.substring(0, i - 1);
+                    language = s.substring(i + 2, s.length());
+                    break;
+                }
+            }
+
+            for (int i = 0; i < nations.length; i++) {
+                if (nations[i].equals(nation)) {
+                    nationalitySpinner.setSelection(i);
+                }
+            }
+            for (int i = 0; i < lans.length; i++) {
+                if (lans[i].equals(language)) {
+                    nativeLanguageSpinner.setSelection(i);
+                }
+            }
+
+        }
+        nameOKTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                profileNameTextView.setText(profileNameEditText.getText().toString());
+                profileNationLanguageTextView.setText(nationalityTextView.getText().toString() + " - " + nativeLanguageTextView.getText().toString());
+                dismiss();
+            }
+        });
+
+        nationalitySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                nationalityTextView.setText(parent.getItemAtPosition(position)+"");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        nativeLanguageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                nativeLanguageTextView.setText(parent.getItemAtPosition(position)+"");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+}
