9af001a8d7ab25c7113943b291e311f48cf2de7a
skysmile3 <isaaclee1101@hanmail.net>
2017-10-28 18:14:02 +0900

    update mypage mainly by rascal (#16)
    Add dialog and image resource

---
+++ app/src/main/java/com/tourwith/koing/Dialog/LanguageDialogListener.java
@@ -0,0 +1,10 @@
+package com.tourwith.koing.Dialog;
+
+/**
+ * Created by MSI on 2017-10-28.
+ */
+
+public interface LanguageDialogListener {
+    public void onPositiveClicked(String language1, String language2, String language3);
+    public void onNegativeClicked();
+}
---
+++ app/src/main/java/com/tourwith/koing/Dialog/MyPageIntroductionDialog.java
@@ -0,0 +1,52 @@
+package com.tourwith.koing.Dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.tourwith.koing.R;
+
+/**
+ * Created by MSI on 2017-10-27.
+ */
+
+public class MyPageIntroductionDialog extends Dialog{
+    private Context context;
+    private static final int LAYOUT = R.layout.mypage_introduction_dialog;
+    private EditText introductionEditText;
+    private TextView introductionOKTextView;
+    private TextView introductionTextView;
+    public MyPageIntroductionDialog(@NonNull Context context, TextView introductionTextView){
+        super(context);
+        this.context = context;
+        this.introductionTextView = introductionTextView;
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(LAYOUT);
+
+        introductionEditText = (EditText)findViewById(R.id.introduction_edit_text);
+        introductionOKTextView = (TextView)findViewById(R.id.introduction_ok_text_view);
+
+        String s = introductionTextView.getText().toString();
+        if(s!=null){
+            introductionEditText.setText(s);
+        }
+        introductionOKTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                introductionTextView.setText(introductionEditText.getText().toString());
+                dismiss();
+            }
+        });
+
+    }
+}
